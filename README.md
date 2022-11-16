6de622f802283a45a9f6928dc64c43b6fdbc611c
unknown <iktasolork22@gmail.com>
2022-11-16 00:24:55 -0700

    added required files

---
+++ README.md
@@ -0,0 +1,16 @@
+# CPNT201 Assignment 4 - NPM Tailwind Assignment
+Author: Rustu Mesut Eser
+
+Live site: [silentchallenger.github.io/cpnt201-a4](https://silentchallenger.github.io/cpnt201-a4)
+
+## Notes
+
+- To use tailwind compiler run `npm run build`
+
+## Attributions
+- [Pexels - Banner Image](https://www.pexels.com/photo/2-motorcycle-racing-on-asphalt-road-during-daytime-62661/)
+- [CPNT 260 Assignment 4: Hero Section with Call-to-Action](https://github.com/silentchallenger/cpnt260-a4)
+- [Font Awesome](https://fontawesome.com/)
+- [Google Fonts](https://fonts.google.com/)
+    - [Roboto](https://fonts.google.com/specimen/Roboto)
+    - [Reem Kufi Fun](https://fonts.google.com/specimen/Reem+Kufi+Fun)
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./*.html",],
+  theme: {
+    extend: {
+      fontFamily: {
+        'reemkufi': ['Reem Kufi Fun', 'Trebuchet MS'],
+        'roboto': ['Roboto', 'Arial']
+      },
+      backgroundImage: {
+        'hero-section': 'linear-gradient(to right bottom, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.7)), url("../images/banner.jpg")'
+      },
+      height: {
+        'hero': '65vh',
+      },
+      colors: {
+        'softorange': '#e9c46a',
+        'darkblue': '#2a9d8f',
+        'darkred': '#e76f51',
+        'darknavy': '#264653'
+      },
+      fontSize: {
+        dynamicH: "clamp(64px, 10vw, 108px)",
+        dynamicP: "clamp(20px, 3vw, 32px)",
+        dynamicA: "clamp(18px, 2vw, 22px)",
+        dynamicBase: "clamp(16px, 2vw, 18px)"
+      },
+      maxWidth: {
+        '30': '30ch',
+        '40': '40ch',
+        '800': '800px'
+      },
+      boxShadow: {
+        'button': '0px 0px 12px 3px #4FA39F',
+      },
+      screens: {
+        '45em': '45em',
+      }
+    },
+  },
+  plugins: [],
+}
